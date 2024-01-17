84d080b926a61a2a516934d38d69d42fa9cb0947
Busenur Adibelli <busenur.adibelli@networksolutionworld.com>
2024-01-17 16:48:29 +0300

    start

---
+++ config/plugins.js
@@ -0,0 +1,22 @@
+// module.exports = ({ env }) => ({
+//     // ...
+//     email: {
+//         config: {
+//             provider: "nodemailer",
+//             providerOptions: {
+//                 host: env("SMTP_HOST", "smtp-mail.outlook.com"),
+//                 port: env("SMTP_PORT", 587),
+//                 auth: {
+//                     user: "info@depoantalya.com",
+//                     pass: "Ss182Y6603",
+//                 },
+//                 // ... any custom nodemailer options
+//             },
+//             settings: {
+//                 defaultFrom: "info@depoantalya.com",
+//                 defaultReplyTo: "info@depoantalya.com",
+//             },
+//         },
+//     },
+//     // ...
+// });
---
+++ src/extensions/app.js
@@ -0,0 +1,53 @@
+import Logo from './extensions/Logo.png';
+import Favicon from './extensions/Favicon.png';
+
+const config = {
+  auth: {
+    logo: Logo,
+  },
+  head: {
+    favicon: Favicon,
+  },
+  menu: {
+    logo: Logo
+  },
+  tutotials: false,
+  notifications: { release: false },
+  locales: [
+    'ar',
+    'fr',
+    'cs',
+    'de',
+    'dk',
+    'es',
+    'he',
+    'id',
+    'it',
+    'ja',
+    'ko',
+    'ms',
+    'nl',
+    'no',
+    'pl',
+    'pt-BR',
+    'pt',
+    'ru',
+    'sk',
+    'sv',
+    'th',
+    'tr',
+    'uk',
+    'vi',
+    'zh-Hans',
+    'zh',
+  ],
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
